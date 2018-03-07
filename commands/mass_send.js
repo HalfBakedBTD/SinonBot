d32003702d7ee6cb267ca5d331a3b070e8d0d2f1
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-03-06 22:16:16 -0500

    Update mass_send.js

--- commands/mass_send.js
+++ commands/mass_send.js
@@ -1,9 +1,11 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    if (message.author.id === "346687165868015616") {
-        message.guild.members.forEach(member => member.send('Thank you for adding Sinon to your discord server!\n\nTo get started type `s!help`.\n\nPlease support those who worked hard on making the bot! \nConsider joining https://discord.gg/euDVUuW which is the owner\'s discord.\n`(there is chance you are accidentally recieving this message due to messed up code. If that is the case, then sorry about that!)`'));
-	}
+	if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("Sorry, you don't have permissions to use this!");
+	const sayMessage = args.join(" ");
+    message.delete().catch(O_o=>{}); 
+    message.channel.send(`Sending users message:\n\t${sayMessage}`);
+    message.guild.members.forEach(member => member.send(`Message from: <@${message.author.id}>:\n\t${sayMessage}`));
 }
  
 module.exports.help = {
