5937dc334f32dc34a7d8159a7b25c8b560c412d3
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2019-07-23 22:18:22 -0400

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    return message.channel.send("**Sinon's  Invite Link:**\n\t :link: https://discordapp.com/api/oauth2/authorize?client_id=420031118088077314&permissions=8&scope=bot :link:")
+    return message.channel.send("**Sinon's  Invite Link:**\n\t :link: https://discordapp.com/api/oauth2/authorize?client_id=603405131773640724&permissions=8&scope=bot :link:")
 }
  
 module.exports.help = {
